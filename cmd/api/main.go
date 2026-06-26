9fa9977a551a4f632f117b8d5350032d1007921c
Pratik Dev <pratikdevofficial1@gmail.com>
2026-06-26 18:12:56 +0600

    render deployment setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -30,7 +30,7 @@ func main() {
         handlers.GetURL(w, r, pool, validate)
     })
 
-    http.ListenAndServe(":" + os.Getenv("API_PORT"), middleware.LoggingMiddleware(
+    http.ListenAndServe(":" + os.Getenv("PORT"), middleware.LoggingMiddleware(
         middleware.RateLimitMiddleware(mux),
         logger,
     ))
